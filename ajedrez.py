6299288b6ba84be4e38d6e93cb59f1246c47bdc8
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2021-12-11 11:38:54 +0100

    cambio

---
+++ ajedrez.py
@@ -0,0 +1,6 @@
+import io
+tablero_inicial='\u265c\t\u265e\t\u265d\t\u265b\t\u265a\t\u265d\t\u265e\t\u265c\n\u265f\t\u265f\t\u265f\t\u265f\t\u265f\t\u265f\t\u265f\t\u265f\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\n\t\t\t\t\t\t\t\t\n\u2659\t\u2659\t\u2659\t\u2659\t\u2659\t\u2659\t\u2659\t\u2659\n\u2656\t\u2658\t\u2657\t\u2655\t\u2654\t\u2657\t\u2658\t\u2656'
+tablero=[]
+movimiento=0
+print(tablero_inicial)
+
